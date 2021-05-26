23b15b9d9e790459991eaff97e74bf1eeb42c21d
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-05-26 14:20:35 +0200

    comments all the log cmd

--- js/script.js
+++ js/script.js
@@ -6,18 +6,19 @@ var nome, cognome, colorePreferito, risultatoConcat;
 
 // lettura del nome
 nome = prompt('Inserisci il tuo nome');
-console.log(nome);
+// console.log(nome);
 
 // lettura del cognome
 cognome = prompt('Inserisci il tuo cognome');
-console.log(cognome);
+// console.log(cognome);
 
 // lettura del colore preferito
 colorePreferito = prompt('Inserisci il tuo colore preferito');
-console.log(colorePreferito);
+// console.log(colorePreferito);
 
 // scrittura e visualizzazione della concatenazione
 risultatoConcat = nome + cognome + colorePreferito + '21';
-console.log(risultatoConcat);
+// console.log(risultatoConcat);
 
+// visualizzazione risultato in html
 document.getElementById('pwd-gen').innerHTML += risultatoConcat;
