8180a350d6c2791ca5df5be58a68b649d5cee4ac
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-05-26 14:19:48 +0200

    add script and text in html

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log('funziona?');
+// check se funziona il collegamento con js
+// console.log('funziona?');
+
+// dichiarazione delle variabili
+var nome, cognome, colorePreferito, risultatoConcat;
+
+// lettura del nome
+nome = prompt('Inserisci il tuo nome');
+console.log(nome);
+
+// lettura del cognome
+cognome = prompt('Inserisci il tuo cognome');
+console.log(cognome);
+
+// lettura del colore preferito
+colorePreferito = prompt('Inserisci il tuo colore preferito');
+console.log(colorePreferito);
+
+// scrittura e visualizzazione della concatenazione
+risultatoConcat = nome + cognome + colorePreferito + '21';
+console.log(risultatoConcat);
+
+document.getElementById('pwd-gen').innerHTML += risultatoConcat;
